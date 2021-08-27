1ebf95b8eec67bf4a42a46c2f09d3ed38b8c201d
Sven Andre Svensson <sven+git@bringsrud.net>
2021-08-27 16:52:37 +0200

    Initial Commit

---
+++ SOT/SOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOTApp.swift
+//  SOT
+//
+//  Created by Sven Svensson on 27/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
